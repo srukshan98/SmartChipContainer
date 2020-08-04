bb259584af94438870c032a1071abc2674851f9f
Sachith Rukshan <rukshan@datacaptureexperts.com.au>
2020-08-04 09:46:39 +0530

    conflict check

--- projects/ngs-chip-container/src/test.ts
+++ projects/ngs-chip-container/src/test.ts
@@ -21,6 +21,6 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
+const context = require.context('./', true, /\.speces\.ts$/);
 // And load the modules.
 context.keys().map(context);
